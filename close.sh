f5bd24da7b07010cfa858e0914bb5121a26b6c75
realdfender <43970103+realdfender@users.noreply.github.com>
2018-10-08 21:29:22 +0200

    Update close.sh

--- close.sh
+++ close.sh
@@ -1,10 +1,11 @@
-!/bin/bash
+#!/bin/bash
 retval=`curl -b gogocookie.txt http://gogogatelocalIP/isg/statusDoor.php?numdoor=1`
 
 if [ "$retval" == "OK" ]
     then
         curl -d 'login=userlogin&pass=userpassword&send-login=submit' -c gogocookie.txt http://gogogatelocalIP/index.php
         curl -b gogocookie.txt http://gogogatelocalIP/isg/opendoor.php?numdoor=1
+        echo CLOSING
     else
         echo CLOSED
 fi
