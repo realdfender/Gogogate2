d4df921bb26864a07908b653ebdf0b11b5247248
realdfender <43970103+realdfender@users.noreply.github.com>
2018-10-08 21:30:05 +0200

    Update open.sh

--- open.sh
+++ open.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 retval=`curl -b gogocookie.txt http://gogogatelocalIP/isg/statusDoor.php?numdoor=1`
 
 if [ "$retval" == "OK" ]
@@ -7,4 +7,5 @@ if [ "$retval" == "OK" ]
     else
         curl -d 'login=userlogin&pass=userpassword&send-login=submit' -c gogocookie.txt http://gogogatelocalIP/index.php
         curl -b gogocookie.txt http://gogogatelocalIP/isg/opendoor.php?numdoor=1
+        echo OPENING
 fi
